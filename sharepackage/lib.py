473d1769586dc4d249b34012e190726d94b62a4d
Will <wilhelmjgriffiths@gmail.com>
2021-05-31 15:19:05 +0100

    test

--- sharepackage/lib.py
+++ sharepackage/lib.py
@@ -1,6 +1,6 @@
 import random
 import string
-print('Call try_me() with any random number to generate a random 30 character password')
+
 characters = string.ascii_letters+string.digits+string.punctuation
 def try_me(random_seed=420):
     emptystr = ''
@@ -8,4 +8,7 @@ def try_me(random_seed=420):
     for i in range(30):
         emptystr += characters[random.randint(0, len(characters)-1)]
     return emptystr
-      
+
+if __name__ == "__main__":
+    # Le Wagon location
+return 'Call try_me() with any random number to generate a random 30 character password'     
