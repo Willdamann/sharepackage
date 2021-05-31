2ed02c16dffb007851cfc844825d59b9f02ef306
Will <wilhelmjgriffiths@gmail.com>
2021-05-31 14:51:39 +0100

    small update

--- sharepackage/lib.py
+++ sharepackage/lib.py
@@ -1,10 +1,11 @@
 import random
 import string
+print('Call try_me() with any random number to generate a random 30 character password')
 characters = string.ascii_letters+string.digits+string.punctuation
 def try_me(random_seed=420):
     emptystr = ''
     random.seed(random_seed)
-    for i in range(12):
+    for i in range(30):
         emptystr += characters[random.randint(0, len(characters)-1)]
     return emptystr
       
